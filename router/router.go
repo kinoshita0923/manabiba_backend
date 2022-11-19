1dc19dc796aad98fd45b27a6b85e4be0c28457d8
kinoshita0923 <sota0923ks15@gmail.com>
2022-11-19 06:27:31 +0000

    ✨ 静的サイトを返すルーティング処理の作成

---
+++ router/router.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+  // インスタンスを作成
+  e := echo.New()
+
+  // ミドルウェアを設定
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // 静的サイトを返すルーティングの設定
+  e.Static("/", "../../frontend/dist/")
+
+  // サーバーをポート番号1323で起動
+  e.Logger.Fatal(e.Start(":1323"))
+}
