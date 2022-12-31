f6cff8773fcdd35795666d1a8466000835f131b2
kinoshita0923 <sota0923ks15@gmail.com>
2022-12-31 12:20:23 +0000

    ⚡️ 認証機能を実装

--- src/main.go
+++ src/main.go
@@ -20,7 +20,7 @@ func main() {
 
   // APIのルーティング
   e.POST("/user/register", user.Register)
-  // e.POST("/user/authentication", user.Authentication)
+  e.POST("/user/authentication", user.Authentication)
   // e.GET("/user/check-login", user.CheckLogin)
   // e.POST("/group/register", group.Register)
   // e.POST("/group/participate", group.Participate)
