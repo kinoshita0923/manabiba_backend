65196610c41a40c9a54fe048bb8e709e15984098
kinoshita0923 <sota0923ks15@gmail.com>
2023-01-03 06:29:55 +0000

    ⚡️ Reactのルーティングを追加

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "src/handlers/user"
+  // "src/handlers/user"
 
   "github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,17 +15,22 @@ func main() {
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
   e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-    AllowOrigins: []string{"http://13.231.249.243"},
+    AllowOrigins: []string{"*"},
     AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
   }))
 
   // 静的サイトを返すルーティングの設定
   e.Static("/docs/", "../docs/")
 
+  e.Static("/", "../frontend/dist")
+  e.Static("/login", "../frontend/dist")
+  e.Static("/signup", "../frontend/dist")
+  e.Static("/search-group", "../frontend/dist")
+
   // APIのルーティング
-  e.POST("/user/register", user.Register)
-  e.POST("/user/authentication", user.Authentication)
-  e.GET("/user/check-login", user.CheckLogin)
+  // e.POST("/user/register", user.Register)
+  // e.POST("/user/authentication/", user.Authentication)
+  // e.GET("/user/check-login", user.CheckLogin)
   // e.POST("/group/register", group.Register)
   // e.POST("/group/participate", group.Participate)
   // e.GET("/group/select", group.Select)
@@ -43,5 +48,5 @@ func main() {
   // e.PUT("/good", good.Reverse)
 
   // サーバーをポート番号8080で起動
-  e.Logger.Fatal(e.Start(":8080"))
+  e.Logger.Fatal(e.Start(":8081"))
 }
