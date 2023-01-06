a2b5ed4ba55472799f5723f60c52400827502ed7
Yamaguchi5923 <1197460@st.anan-nct.ac.jp>
2023-01-06 16:31:21 +0000

    グループ新規登録APIを実装

--- src/handlers/group/group_handler.go
+++ src/handlers/group/group_handler.go
@@ -1,12 +1,39 @@
 package group
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"src/database"
 )
 
-// func Register(c echo.Context) error {}
+ func Register(c echo.Context) error {
+	// 値を変数に格納
+	name := c.FormValue("group_name")
+
+	// データベースのハンドルを取得
+	db := database.Connect()
+	defer db.Close()
+
+	// SQL文を定義
+	insert, err := db.Prepare("INSERT INTO groups(group_name) VALUES(?);")
+	if err != nil{
+		log.Fatal(err)
+		return c.NoContent(http.StatusOK)
+	}
+	defer insert.Close()
+
+	// SQLの実行
+	_, err = insert.Exec(name)
+	if err != nil{
+		log.Fatal(err)
+		return c.NoContent(http.StatusOK)
+	}
+
+	return c.NoContent(http.StatusOK)
+ }
 
 // func Participate(c echo.Context) error {}
 
