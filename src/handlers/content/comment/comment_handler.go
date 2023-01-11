3f07a76c338286e6a68758bf730c58dc1bd49775
kinoshita0923 <sota0923ks15@gmail.com>
2023-01-11 11:15:11 +0000

    ✨ 口コミ投稿APIを実装

--- src/handlers/content/comment/comment_handler.go
+++ src/handlers/content/comment/comment_handler.go
@@ -1,11 +1,51 @@
 package comment
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"src/database"
+	"src/lib/jwt"
 )
 
-// func Register(c echo.Context) error {}
+func Register(c echo.Context) error {
+	subjectId	:= c.FormValue("subject_id")
+	valuation	:= c.FormValue("valuation")
+	comment		:= c.FormValue("comment")
+	teacherName	:= c.FormValue("teacher_name")
+	term		:= c.FormValue("term")
+	studyTime	:= c.FormValue("study_time")
+
+	// ユーザIDをトークンから取得
+	tokenCookie, err := c.Cookie("token")
+	if err != nil {
+		return c.String(http.StatusOK, "No token")
+	}
+	tokenText := tokenCookie.Value
+	userId := jwt.ParseToken(tokenText).(float64)
+
+	// データベースのハンドルを取得
+	db := database.Connect()
+	defer db.Close()
+
+	// SQL文を定義
+	insert, err := db.Prepare("INSERT INTO evaluations(subject_id, user_id, valuation, comment, teacher_name, term, study_time) VALUES(?, ?, ?, ?, ?, ?, ?);")
+	if err != nil {
+		log.Fatal(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	defer insert.Close()
+
+	// SQLの実行
+	_, err = insert.Exec(subjectId, userId, valuation, comment, teacherName, term, studyTime)
+	if err != nil{
+		log.Fatal(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.NoContent(http.StatusCreated)
+}
 
 // func Update(c echo.Context) error {}
--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "src/handlers/user"
   "src/handlers/group"
+  "src/handlers/content/comment"
 
   "github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -40,7 +41,7 @@ func main() {
   // e.POST("/subject/register", subject.Register)
   // e.GET("/subject/select", subject.Select)
   // e.POST("/point/add-difference", point.AddDifference)
-  // e.POST("/content/comment/register", comment.Register)
+  e.POST("/content/comment/register", comment.Register)
   // e.PATCH("/content/comment/update", comment.Update)
   // e.POST("/content/examination/register", examination.Register)
   // e.PATCH("/content/examination/update", examination.Update)
