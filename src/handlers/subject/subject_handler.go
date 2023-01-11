465a9649bbf185517f8b3f622efd1e15435cd5ab
kinoshita0923 <sota0923ks15@gmail.com>
2023-01-11 13:19:44 +0000

    ✨ 授業を取得するAPIを実装

--- src/handlers/subject/subject_handler.go
+++ src/handlers/subject/subject_handler.go
@@ -1,11 +1,57 @@
 package subject
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"src/database"
 )
+
+type Subject struct {
+	SubjectId	int
+	SubjectName	string
+}
+
+type Subjects struct {
+	Subjects	[]Subject
+}
 
 // func Register(c echo.Context) error {}
 
-// func Select(c echo.Context) error {}
+func LoadSubject(c echo.Context) error {
+	classId := c.QueryParam("class_id")
+
+	// データベースのハンドルを取得
+	db := database.Connect()
+	defer db.Close()
+
+	rows, err := db.Query(
+		"SELECT subjects.subject_id, subjects.subject_name FROM subjects NATURAL JOIN lesson_relations WHERE lesson_relations.class_id = ?;",
+		classId,
+	)
+	if err != nil {
+		log.Fatal(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	defer rows.Close()
+
+	subjects := &Subjects{}
+	for rows.Next() {
+		subject := &Subject{}
+		err := rows.Scan(&subject.SubjectId, &subject.SubjectName)
+
+		if len(subjects.Subjects) == 0 {
+			subjects.Subjects = []Subject{*subject}
+		} else {
+			subjects.Subjects = append(subjects.Subjects, *subject)
+		}
+
+		if err != nil {
+			return c.NoContent(http.StatusInternalServerError)
+		}
+	}
+
+	return c.JSON(http.StatusOK, subjects)
+}
